7eceb9265e1271546f431b13e4d53eb6a7a1df6c
ubergesundheit <ubergesundheit@googlemail.com>
2014-11-17 20:30:00 +0100

    api returns geojson, osm data

--- lib/parking_api.rb
+++ lib/parking_api.rb
@@ -5,17 +5,18 @@ require 'time'
 
 class ParkingApi
   def self.call(env)
-    [200, {"Content-Type"=>"application/json"}, StringIO.new(self.get_parking_spaces)]
+    [200, {"Content-Type"=>"application/json; charset=utf-8"}, StringIO.new(self.get_parking_spaces)]
   end
 
-  CACHE_MAX_AGE = 120
+  CACHE_MAX_AGE = 60
   @base_url = 'http://www5.stadt-muenster.de/parkhaeuser/'
+  @geojson = JSON.parse(open(File.join(File.dirname(File.expand_path(__FILE__)), 'parking_spaces.json')).read)
   @cache = {
-    time: 0
+    fetch_time: 0
   }
 
   def self.get_parking_spaces
-    if Time.now.to_i - @cache[:time] < CACHE_MAX_AGE
+    if Time.now.to_i - @cache[:fetch_time] < CACHE_MAX_AGE
       @cache.to_json
     else
       self.build_cache().to_json
@@ -23,20 +24,27 @@ class ParkingApi
   end
 
   def self.build_cache
-    parking_spaces = []
+    parking_spaces = @geojson["features"].clone
+
     doc = Nokogiri::HTML(open(@base_url))
+    fetch_time = Time.now.to_i
+
     doc.css('area').each do |area|
-      parking_spaces << {
-        name: area['alt'],
-        free: area['onclick'][/frei=[\d]+/].split('=')[1],
-        total: area['onclick'][/gesamt=[\d]+/].split('=')[1],
-        status: area['onclick'][/status=\w+/].split('=')[1]
-      }
+      index = parking_spaces.index { |f| f["properties"]["name"] == area["alt"] }
+
+      unless index == nil
+        parking_spaces[index]["properties"].merge!({
+          free: area['onclick'][/frei=[\d]+/].split('=')[1],
+          total: area['onclick'][/gesamt=[\d]+/].split('=')[1],
+          status: area['onclick'][/status=\w+/].split('=')[1]
+        })
+      end
     end
-    @cache = {
-      time: Time.parse(doc.css('strong')[0].text).to_i,
-      parking_spaces: parking_spaces
-    }
+
+    @cache = @geojson.clone
+    @cache["features"] = parking_spaces
+    @cache[:timestamp] = Time.parse(doc.css('strong')[0].text).to_i
+    @cache[:fetch_time] = fetch_time
     @cache
   end
 end
